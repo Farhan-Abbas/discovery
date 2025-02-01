d561c9027ca1a6c417538eb81813f67b2b985ae7
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-02-01 14:35:43 -0700

    updated

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,19 +2,32 @@
 #![no_main]
 #![no_std]
 
-use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile::Volatile;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let half_period = 500_u16;
+    let mut period = 50_u16;
+    // let v_half_period = Volatile::new(&mut half_period);
+    let mut index = 0;
+    leds[index].on().ok();
+    delay.delay_ms(period);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(half_period);
-
-        leds[0].off().ok();
-        delay.delay_ms(half_period);
+        delay.delay_ms(period);
+        index += 1;
+        if (index == 8) {
+            index = 0;
+        }
+        leds[index].on().ok();
+        delay.delay_ms(period);
+        if index == 0 {
+            leds[7].off().ok();
+        } else {
+            leds[index-1].off().ok();
+        }
     }
 }
+
