fba8f131a77f2932e8bebad0c3261b46835a9209
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-03-22 14:12:39 -0600

    completed chapter 16

--- f3discovery/src/16-punch-o-meter/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![no_std]
-/// Package config contains the implementation of Code for Compass in initialization file.
-pub mod config {
-    pub mod initialization;
-}
--- f3discovery/src/16-punch-o-meter/src/main.rs
+++ f3discovery/src/16-punch-o-meter/src/main.rs
@@ -1,55 +1,20 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-use cortex_m::asm;
+#[allow(unused_imports)]
+use aux16::{entry, iprint, iprintln, prelude::*, I16x3, Sensitivity};
 use m::Float;
-use stm32f3_Accelerometer::config::initialization::*;
-use stm32f3_discovery::stm32f3xx_hal::hal::blocking::delay::DelayMs;
 
-/// This program is going to print the Accelerometer sensor readings(x, y, z-axis) on the itm-dump console with
-/// 9.26 sec of delay.
-///
-/// #Arguments
-/// None
-///
-/// #Return
-/// This is a no_std no_main code which will neither return nor stop.
 #[entry]
 fn main() -> ! {
     const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
     const THRESHOLD: f32 = 0.5;
-    let (mut lsm303dlhc, mut delay, mut itm, mono_timer) = initialization();
-    match lsm303dlhc.set_accel_sensitivity(Sensitivity::G12) {
-        Ok(senstivity) => senstivity,
-        Err(err) => panic!("Can't set accelerometer senstivity {:?}", err),
-    }
-    // Get the Acceleromter Sensor Readings....
-    loop {
-        const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
 
-        let I16x3 { x, y, z } = match lsm303dlhc.accel() {
-            Ok(values) => values,
-            Err(error) => panic!("Cannot get sensor readings {:?}", error),
-        };
+    let (mut lsm303dlhc, mut delay, mono_timer, mut itm) = aux16::init();
 
-        let x_axis = x as f32 * SENSITIVITY;
-        let y_axis = y as f32 * SENSITIVITY;
-        let z_axis = z as f32 * SENSITIVITY;
+    lsm303dlhc.set_accel_sensitivity(Sensitivity::G12).unwrap();
 
-        iprintln!(
-            &mut itm.stim[0],
-            "Accelerometer Readings \n x= {:?} y= {:?} z= {:?}",
-            x_axis,
-            y_axis,
-            z_axis
-        );
-
-        //9.26seconds of delay--
-        asm::delay(50_000_000)
-    }
-    
-    // uncomment this to build the puncho-o-meter.
-    // CODE FOR Punch-o-meter....
     let measurement_time = mono_timer.frequency().0; // 1 second in ticks
     let mut instant = None;
     let mut max_g = 0.;
